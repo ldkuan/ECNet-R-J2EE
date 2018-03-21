2c2487ee33e943888464a994750a19a0393297db
sweets <141250073@smail.nju.edu.cn>
2018-03-21 11:27:59 +0800

    mysql

--- src/main/java/com/ecm/model/Evidence_Document.java
+++ src/main/java/com/ecm/model/Evidence_Document.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 public class Evidence_Document {
 
         @Id
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
         @Column(name = "id")
         private int id;
         @Column(name = "ajxh")
--- src/main/java/com/ecm/service/EvidenceService.java
+++ src/main/java/com/ecm/service/EvidenceService.java
@@ -2,11 +2,22 @@ package com.ecm.service;
 
 import com.ecm.model.Evidence_Body;
 import com.ecm.model.Evidence_Document;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 public interface EvidenceService{
 
     public Evidence_Document save(Evidence_Document evidence_document);
 
     public Evidence_Body save(Evidence_Body evidence_body);
+
+    public void deleteById(int id);
+
+
+    public void updateBodyById(String body, int id);
+
+    public void updateTypeById(int type, int id);
 
 }
