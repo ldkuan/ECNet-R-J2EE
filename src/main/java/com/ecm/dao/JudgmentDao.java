71cdce2ffb02b9b69e5627d3b41b18d4084b2723
DemonHe <devilzoey@163.com>
2018-03-21 11:32:23 +0800

    修复bug

--- src/main/java/com/ecm/dao/JudgmentDao.java
+++ src/main/java/com/ecm/dao/JudgmentDao.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface JudgmentDao extends JpaRepository<Judgment, String> {
 
-    @Query(value = "select j from Judgment j where j.realName = ?1 and (j.isUndertaker= 'Y' OR j.hasJudge='0')")
+    @Query(value = "select j from Judgment j where j.realName = ?1 and (j.isUndertaker= 'Y' OR j.isJudge='0')")
     public List<Judgment> getAllByName(String name);
 }
