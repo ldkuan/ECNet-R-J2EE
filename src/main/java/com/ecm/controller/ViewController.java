66551309d7727bdb82227e39bd8b1294aa8d366d
noisyjonah <1119407797@qq.com>
2018-04-18 15:06:36 +0800

    zpl 0418

--- src/main/java/com/ecm/controller/ViewController.java
+++ src/main/java/com/ecm/controller/ViewController.java
@@ -6,17 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ViewController {
 
-    @RequestMapping(value = "/login")
-    public String getLoginView() {
-        return "login";
-    }
+	@RequestMapping(value = "/login")
+	public String getLoginView() {
+		return "login";
+	}
 
+	@RequestMapping(value = "/model")
+	public String getModelView() {
+		return "model";
+	}
 
-    @RequestMapping(value = "/model")
-    public String getModelView() {
-        return "model";
-    }
-
-
-
+	@RequestMapping(value = "/logic")
+	public String getLogicView() {
+		return "logic";
+	}
 }
